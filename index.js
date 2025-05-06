11d94d7b4ed603b005110c8fa661533bb2639406
TheFuryk <142916467+TheFuryk@users.noreply.github.com>
2025-05-06 23:43:22 +0500

    finalized

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+
+
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
+let sumEl = document.getElementById("sum-el")
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+// 1. Create a function, getRandomCard(), that always returns the number 5"
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
-
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function getRandomCard() {
+      // if 1     -> return 11
+    // if 11-13 -> return 10
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber=== 1){
+        return 11
+    }
+    else if ( randomNumber > 10){
+        return 10 
+    }
+    else{
+        return randomNumber
+    }
 }
-console.log(message)
+
+function startGame() {
+    sum =0
+    cards = []
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard =getRandomCard()
+    cards.push(firstCard, secondCard)
+    sum = firstCard + secondCard
+   
+
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: "  // Reset and label once
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+        // 2. Display the message in the messageEl using messageEl.textContent
+        messageEl.textContent = message
+        // 3. Call the startGame() function to start the game
+        
+        
+}
+
+
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+   // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive=== true && hasBlackJack === false){
+        let card = getRandomCard()
+        cards.push(card)
+        sum += card
+        renderGame()
+    }
+
+    } 
